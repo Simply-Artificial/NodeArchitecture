b3f3d72c1d0855ae5f502a8e278b4300fe903f0f
alf1e <alfiewbarlow@gmail.com>
2023-04-27 17:49:07 +0100

    Update: Template

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     license='MIT',
     packages=[
         'node_architecture',
+        'node_architecture.Server'
     ],
     install_requires=open('requirements.txt').read().splitlines(),
 
